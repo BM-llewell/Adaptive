75b9555349f3755b65c10359049222487694f7f2
BM-llewell <BM-llewell@users.noreply.github.com>
2019-09-07 15:36:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,51 @@ $(".menu-open").click(function() {
 // 		$(".open-burger").toggleClass('bactive');
 // 	}
 // })
+
+google.charts.load('current', {'packages':['corechart']});
+
+      google.charts.setOnLoadCallback(drawRecord);
+		google.charts.setOnLoadCallback(drawAppeal);
+
+      function drawRecord() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Отклонённых', 1],
+          ['Принятых', 2],
+          ['В ожидании', 3],
+        ]);
+
+        var options = {'title':'Запись на приём',
+					   'legend': 'bottom',
+					   'colors': ['#ff0000', '#009000', '#adadad'],
+                       'width':600,
+                       'height':300};
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_records'));
+        chart.draw(data, options);
+		document.getElementById('chart_records').getElementsByTagName('text')[0].setAttribute('x', '250');
+      }
+	function drawAppeal() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Соотвутствуют', 1],
+          ['Нарушения', 2],
+          ['Заниженная', 3],
+        ]);
+
+        var options = {'title':'Мониторинг цен',
+					   'legend': 'bottom',
+					   'colors': ['#adadad', '#ff0000', '#009000'],
+                       'width':600,
+                       'height':300};
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_appeals'));
+        chart.draw(data, options);
+		document.getElementById('chart_appeals').getElementsByTagName('text')[0].setAttribute('x', '250');
+      }
