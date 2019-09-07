e79f35dd1452daf38c2f7e94d6ffa31d573c3107
BM-llewell <BM-llewell@users.noreply.github.com>
2019-09-07 12:25:04 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ $(".menu-open").click(function() {
 	$(".main-menu").toggleClass('active');
 	$(".container").toggleClass('cactive');
 	$(".open-burger").toggleClass('bactive');
+})
+
+// $(document).keydown(function(e) {
+// 	if(e.keyCode == 77) {
+// 		$(".main-menu").toggleClass('active');
+// 		$(".container").toggleClass('cactive');
+// 		$(".open-burger").toggleClass('bactive');
+// 	}
 // })
